5c714ba58d012827e9d5cf9dae88d450490af2f3
vishalJ-addweb <vishal.j@addwebsolution.in>
2021-12-29 17:08:11 +0530

    mui-task moved in new repo

--- material-ui-task/src/components/routesComponents/Hoc.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Hoc = () => {
-  return <div>This is HOC Pattern.</div>;
-};
-
-export default Hoc;
--- material-ui-task/src/components/routesComponents/Home.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Home = () => {
-  return <div>This is Home Page</div>;
-};
-
-export default Home;
